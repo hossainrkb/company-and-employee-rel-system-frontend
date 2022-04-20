c5d95c1547cf4a112068efba00af1a3549ff4469
Rakib Hosen <rakib.151045@gmail.com>
2022-04-20 17:27:36 +0600

    add emp navbar

---
+++ src/Project/Navbars/EmployeeNavbar.js
@@ -0,0 +1,96 @@
+import React, { Component, useState, useEffect } from "react";
+import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import EmployeeInfoContext from "../Component/Context/EmployeeInfoContext";
+function Header({ handleLogoutEmployee }) {
+  const mobileSidebarToggle = (e) => {
+    e.preventDefault();
+    document.documentElement.classList.toggle("nav-open");
+    var node = document.createElement("div");
+    node.id = "bodyClick";
+    node.onclick = function () {
+      this.parentElement.removeChild(this);
+      document.documentElement.classList.toggle("nav-open");
+    };
+    document.body.appendChild(node);
+  };
+
+  const handleLogout = async () => {
+    handleLogoutEmployee();
+  };
+
+  const getBrandText = () => {
+    return "Employee";
+  };
+
+  return (
+    <Navbar expand="lg default_style_two">
+      <Container fluid>
+        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
+          <Button
+            variant="dark"
+            className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
+            onClick={mobileSidebarToggle}
+          >
+            <i className="fas fa-ellipsis-v"></i>
+          </Button>
+          <Navbar.Brand
+            href="#home"
+            onClick={(e) => e.preventDefault()}
+            className="mr-2"
+          >
+            <i className="nc-icon white_color nc-palette"></i> &nbsp;
+            <NavLink to="/" className="white_color">
+              {getBrandText()}
+            </NavLink>
+          </Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
+          <span className="navbar-toggler-bar burger-lines"></span>
+          <span className="navbar-toggler-bar burger-lines"></span>
+          <span className="navbar-toggler-bar burger-lines"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav mr-auto" navbar>
+            <Nav.Item>
+              <Nav.Link
+                data-toggle="dropdown"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+                className="m-0"
+              ></Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <EmployeeInfoContext.Consumer>
+                  {({ employeeInfo }) => {
+                    return (
+                      <span className="no-icon white_color">
+                        <b> {employeeInfo ? employeeInfo.name : "Guest"}</b>
+                      </span>
+                    );
+                  }}
+                </EmployeeInfoContext.Consumer>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className="m-0" onClick={(e) => handleLogout()}>
+                <span className="no-icon white_color">
+                  <b>Log out</b>
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
