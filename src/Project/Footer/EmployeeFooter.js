aff74af3e97261e210e18d47ffed7ebd37a5a483
Rakib Hosen <rakib.151045@gmail.com>
2022-04-20 17:27:25 +0600

    add emp footer

---
+++ src/Project/Footer/EmployeeFooter.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class EmployeeFooter extends Component {
+  render() {
+    return (
+      <footer className="footer px-0 px-lg-3">
+        <Container fluid>
+          <nav>
+            <p className="copyright text-center">
+              © {new Date().getFullYear()}{" "}
+             <span>Void</span>
+            </p>
+          </nav>
+        </Container>
+      </footer>
+    );
+  }
+}
+
+export default EmployeeFooter;
